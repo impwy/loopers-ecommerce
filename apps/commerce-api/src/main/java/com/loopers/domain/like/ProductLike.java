69f80831c96ef47f6fbc20ca894c3f017cfdc1ee
wonyong Park <106656666+impwy@users.noreply.github.com>
2025-07-29 22:15:45 +0900

    Feature/like (#13)
    * feat: 상품 좋아요 생성 추가
    
    * feat: 상품 좋아요 생성 통합테스트 추가
    
    * feat: 좋아요 생성 단위테스트 추가
    
    * fix: 충돌 제거
    
    * feat: 상품 좋아요 멱등성 추가
    
    * refactor: productFixture 추가
    
    * feat: 상품 좋아요 취소 단위테스트 추가
    
    * feat: 상품 좋아요 취소 통합테스트 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/like/ProductLike.java
@@ -0,0 +1,54 @@
+package com.loopers.domain.like;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static java.util.Objects.requireNonNull;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.domain.member.Member;
+import com.loopers.domain.product.Product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "product_like")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductLike extends BaseEntity {
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @JoinColumn(name = "product_id")
+    @ManyToOne(fetch = LAZY)
+    private Product product;
+
+    private ProductLike(Member member, Product product) {
+        this.member = requireNonNull(member);
+        this.product = requireNonNull(product);
+    }
+
+    public static ProductLike create(Member member, Product product) {
+        if (member == null) {
+            throw new IllegalArgumentException("유저는 필수 항목입니다.");
+        }
+        if (product == null) {
+            throw new IllegalArgumentException("상품은 필수 항목입니다.");
+        }
+        return new ProductLike(member, product);
+    }
+
+    public boolean isDeleted(ProductLike productLike) {
+        return productLike.getDeletedAt() != null;
+    }
+
+    public boolean isNotDeleted(ProductLike productLike) {
+        return !isDeleted(productLike);
+    }
+}
---
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/like/ProductLikeRepositoryImpl.java
@@ -0,0 +1,26 @@
+package com.loopers.infrastructure.like;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.loopers.application.required.ProductLikeRepository;
+import com.loopers.domain.like.ProductLike;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductLikeRepositoryImpl implements ProductLikeRepository {
+    private final ProductLikeJpaRepository productLikeJpaRepository;
+
+    @Override
+    public ProductLike save(ProductLike productLike) {
+        return productLikeJpaRepository.save(productLike);
+    }
+
+    @Override
+    public Optional<ProductLike> findByMemberIdAndProductId(Long memberId, Long productId) {
+        return productLikeJpaRepository.findByMemberIdAndProductId(memberId, productId);
+    }
+}
