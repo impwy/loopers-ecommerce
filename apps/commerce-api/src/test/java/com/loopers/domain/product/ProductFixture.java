b612918638696210fac825ed0b9470efdcf07a7e
wonyong <pwy6817@gmail.com>
2025-07-29 23:07:22 +0900

    refactor: BrandFixture 추가

--- apps/commerce-api/src/test/java/com/loopers/domain/product/ProductFixture.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/product/ProductFixture.java
@@ -2,8 +2,10 @@ package com.loopers.domain.product;
 
 import java.math.BigDecimal;
 
+import com.loopers.domain.brand.BrandFixture;
+
 public class ProductFixture {
     public static Product createProduct() {
-        return Product.create("상품", "상품입니다.", BigDecimal.valueOf(500));
+        return Product.create("상품", "상품입니다.", BigDecimal.valueOf(500), BrandFixture.createBrand());
     }
 }
