daaee61de253e713e63f8ad583a8d97841a062dc
parkwonyong <pwy6817@gmail.com>
2025-07-30 10:12:33 +0900

    refactor: 상품 브랜드 도메인서비스 수정

--- apps/commerce-api/src/main/java/com/loopers/application/ProductFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/ProductFacade.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.loopers.application.provided.ProductFinder;
+import com.loopers.application.provided.ProductLikeFinder;
+import com.loopers.domain.brand.Brand;
 import com.loopers.domain.product.Product;
 import com.loopers.domain.product.ProductInfo;
-import com.loopers.domain.product.ProductService;
+import com.loopers.domain.product.ProductBrandDomainService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,11 +16,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductFacade {
     private final ProductFinder productFinder;
-    private final ProductService productService;
+    private final ProductLikeFinder productLikeFinder;
+    private final ProductBrandDomainService productBrandDomainService = new ProductBrandDomainService();
 
     @Transactional
     public ProductInfo findProductInfo(Long productId) {
         Product product = productFinder.find(productId);
-        return productService.findProductWithBrand(product);
+        Brand brand = product.getBrand();
+        Long likeCount = productLikeFinder.countByProductId(productId);
+        return productBrandDomainService.findProductWithBrand(product, brand, likeCount);
     }
 }
--- apps/commerce-api/src/main/java/com/loopers/domain/product/ProductInfo.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductInfo.java
@@ -9,10 +9,9 @@ public record ProductInfo(Long productId, Long brandId, String productName, Stri
                           String brandName, String brandDescription, ZonedDateTime productCreatedAt,
                           ZonedDateTime productUpdatedAt, Long likeCount) {
 
-    public static ProductInfo of(Product product, Long likeCount) {
-        Brand brand = product.getBrand();
-
+    public static ProductInfo of(Product product, Brand brand, Long likeCount) {
         return new ProductInfo(product.getId(), brand.getId(), product.getName(), product.getDescription(), product.getPrice(),
-                               brand.getName(), brand.getDescription(), product.getCreatedAt(), product.getUpdatedAt(), likeCount);
+                               brand.getName(), brand.getDescription(), product.getCreatedAt(), product.getUpdatedAt(),
+                               likeCount);
     }
 }
