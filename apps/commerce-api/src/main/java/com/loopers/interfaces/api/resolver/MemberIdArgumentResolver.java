d866cb6298f2ee347ee5bcd1c0ace5da3b4f7a79
parkwonyong <pwy6817@gmail.com>
2025-07-30 14:09:49 +0900

    feat: X_USER_ID 검증 resolver 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/resolver/MemberIdArgumentResolver.java
@@ -0,0 +1,34 @@
+package com.loopers.interfaces.api.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import com.loopers.domain.member.MemberId;
+
+@Component
+public class MemberIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    public static final String X_USER_ID = "X-USER-ID";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(MemberId.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        String header = webRequest.getHeader(X_USER_ID);
+        if (header == null || header.isBlank()) {
+            throw new MissingRequestHeaderException(X_USER_ID, parameter);
+        }
+
+        MemberId memberId = new MemberId(header);
+        return memberId;
+    }
+}
