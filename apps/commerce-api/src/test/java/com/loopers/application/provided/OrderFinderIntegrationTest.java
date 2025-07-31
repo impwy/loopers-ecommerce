55d54404e0508df3491d8129cf02cd23b699391e
wonyong <pwy6817@gmail.com>
2025-07-31 20:25:41 +0900

    test: 주문 조회 통합테스트 추가

---
+++ apps/commerce-api/src/test/java/com/loopers/application/provided/OrderFinderIntegrationTest.java
@@ -0,0 +1,74 @@
+package com.loopers.application.provided;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+
+import com.loopers.application.required.OrderRepository;
+import com.loopers.domain.order.Address;
+import com.loopers.domain.order.CreateOrderSpec;
+import com.loopers.domain.order.Order;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+
+@SpringBootTest
+class OrderFinderIntegrationTest {
+
+    @MockitoSpyBean
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderFinder orderFinder;
+
+    @DisplayName("존재하지 않는 주문 번호 조회 시 NOT_FOUND 반환")
+    @Test
+    void find_order_fail_test() {
+        CoreException coreException = assertThrows(CoreException.class, () -> orderFinder.find(-1L));
+
+        assertThat(coreException.getErrorType()).isEqualTo(ErrorType.NOT_FOUND);
+    }
+
+    @DisplayName("주문 ID로 주문 조회 성공")
+    @Test
+    void find_order_by_order_id() {
+        Order order = orderRepository.save(Order.create(CreateOrderSpec.of(1L, "000000",
+                                                                          new Address("00001", "Seoul", "Seongdong-gu", "Seuongsudong", "etc"))));
+
+        Order expected = orderFinder.find(order.getId());
+
+        assertAll(
+                () -> assertThat(expected.getMemberId()).isEqualTo(order.getMemberId()),
+                () -> assertThat(expected.getOrderNo()).isEqualTo(order.getOrderNo()),
+                () -> assertThat(expected.getAddress()).isEqualTo(order.getAddress())
+        );
+    }
+
+    @DisplayName("존재하지 않는 유저로 조회 시 NOT_FOUND 반환")
+    @Test
+    void find_order_fail_with_memberId_test() {
+        CoreException coreException = assertThrows(CoreException.class, () -> orderFinder.findByMemberId(-1L));
+
+        assertThat(coreException.getErrorType()).isEqualTo(ErrorType.NOT_FOUND);
+    }
+
+    @DisplayName("유저 아이디로 주문 조회 성공")
+    @Test
+    void find_order_by_memberId() {
+        Order order = orderRepository.save(Order.create(CreateOrderSpec.of(1L, "000000",
+                                                                           new Address("00001", "Seoul", "Seongdong-gu", "Seuongsudong", "etc"))));
+
+        Order expected = orderFinder.findByMemberId(order.getMemberId());
+
+        assertAll(
+                () -> assertThat(expected.getMemberId()).isEqualTo(order.getMemberId()),
+                () -> assertThat(expected.getOrderNo()).isEqualTo(order.getOrderNo()),
+                () -> assertThat(expected.getAddress()).isEqualTo(order.getAddress())
+        );
+    }
+}
