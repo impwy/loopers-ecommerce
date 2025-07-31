b02e32bea97e8ae2f914c01aed110ec9683642a2
wonyong <pwy6817@gmail.com>
2025-08-01 01:38:52 +0900

    refactor: 주문 생성 facade 수정

--- apps/commerce-api/src/main/java/com/loopers/application/order/OrderFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/order/OrderFacade.java
@@ -9,14 +9,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.loopers.application.provided.InventoryRegister;
 import com.loopers.application.provided.MemberFinder;
-import com.loopers.application.provided.OrderItemRegister;
 import com.loopers.application.provided.OrderRegister;
 import com.loopers.application.provided.ProductFinder;
 import com.loopers.domain.member.Member;
 import com.loopers.domain.member.MemberId;
 import com.loopers.domain.order.CreateOrderSpec;
 import com.loopers.domain.order.Order;
-import com.loopers.domain.orderitem.CreateOrderItemSpec;
 import com.loopers.domain.product.Product;
 import com.loopers.interfaces.api.order.dto.OrderV1Dto.Request.CreateOrderRequest;
 import com.loopers.interfaces.api.order.dto.OrderV1Dto.Response.OrderProductInfo;
@@ -29,7 +27,6 @@ public class OrderFacade {
     private final OrderRegister orderRegister;
     private final MemberFinder memberFinder;
     private final ProductFinder productFinder;
-    private final OrderItemRegister orderItemRegister;
     private final InventoryRegister inventoryRegister;
 
     @Transactional
@@ -40,26 +37,25 @@ public class OrderFacade {
         CreateOrderSpec createOrderSpec = CreateOrderSpec.of(member.getId());
 
         Order order = orderRegister.register(createOrderSpec);
-        Long orderId = order.getId();
 
         createOrderRequests.forEach(createOrderRequest -> {
-            Product product = productFinder.find(createOrderRequest.productId());
-            Long totalQuantity = createOrderRequest.quantity();
-            BigDecimal quantity = BigDecimal.valueOf(totalQuantity);
-            BigDecimal totalPrice = product.getTotalPrice(quantity);
+            Long requestQuantity = createOrderRequest.quantity();
+
+            Long productId = createOrderRequest.productId();
+            Product product = productFinder.find(productId);
 
 
-            CreateOrderItemSpec createOrderItemSpec = CreateOrderItemSpec.of(orderId,
-                                                                             createOrderRequest.productId(),
-                                                                             totalQuantity,
-                                                                             totalPrice);
-            orderItemRegister.register(createOrderItemSpec);
+            BigDecimal totalPrice = product.getTotalPrice(BigDecimal.valueOf(requestQuantity));
 
-            inventoryRegister.decrease(product.getId(), totalQuantity);
+            order.addOrderItem(productId, requestQuantity, totalPrice);
+
+            inventoryRegister.decrease(product.getId(), requestQuantity);
+
             member.decrease(totalPrice);
 
             OrderProductInfo orderProductInfo = OrderProductInfo.of(order.getOrderNo().value(), product.getName(),
-                                                                    totalQuantity, totalPrice);
+                                                                    requestQuantity, totalPrice);
+
             productInfos.add(orderProductInfo);
         });
 
