718423a11ef4a1d908f025455e077246ddc24a0a
wonyong <pwy6817@gmail.com>
2025-07-31 20:25:29 +0900

    feat: 주문 조회 추가

--- apps/commerce-api/src/main/java/com/loopers/application/required/OrderRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/application/required/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.loopers.application.required;
 
+import java.util.Optional;
+
 import com.loopers.domain.order.Order;
 
 public interface OrderRepository {
     Order save(Order order);
+
+    Optional<Order> find(Long orderId);
+
+    Optional<Order> findByMemberId(Long memberId);
 }
--- apps/commerce-api/src/main/java/com/loopers/infrastructure/order/OrderRepositoryImpl.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/order/OrderRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.loopers.infrastructure.order;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.loopers.application.required.OrderRepository;
@@ -15,5 +17,15 @@ public class OrderRepositoryImpl implements OrderRepository {
     @Override
     public Order save(Order order) {
         return orderJpaRepository.save(order);
+    }
+
+    @Override
+    public Optional<Order> find(Long orderId) {
+        return orderJpaRepository.findById(orderId);
+    }
+
+    @Override
+    public Optional<Order> findByMemberId(Long memberId) {
+        return orderJpaRepository.findByMemberId(memberId);
     }
 }
