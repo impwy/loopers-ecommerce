7b9b6076504be6f0d85775cf887b744aa65aff36
parkwonyong <pwy6817@gmail.com>
2025-07-31 16:37:10 +0900

    feat: 주문 아이템 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/orderitem/OrderItem.java
@@ -0,0 +1,48 @@
+package com.loopers.domain.orderitem;
+
+import java.math.BigDecimal;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem extends BaseEntity {
+    private Long orderId;
+
+    private Long productId;
+
+    private Long quantity;
+
+    private BigDecimal price;
+
+    private OrderItem(Long orderId, Long productId, Long quantity, BigDecimal price) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public static OrderItem create(Long orderId, Long productId, Long quantity, BigDecimal price) {
+        if (orderId == null) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "유저는 필수입니다.");
+        }
+        if (productId == null) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "상품은 필수입니다.");
+        }
+        if (quantity == null || quantity <= 0) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "잘못된 수량입니다. : " + quantity);
+        }
+        if (price == null || price.doubleValue() <= 0) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "잘못된 총 가격입니다. : " + price);
+        }
+        return new OrderItem(orderId, productId, quantity, price);
+    }
+}
