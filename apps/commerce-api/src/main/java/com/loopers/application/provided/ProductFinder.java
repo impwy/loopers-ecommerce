4923c11e562d61a1296fbadf9f265648c6818494
wonyong <pwy6817@gmail.com>
2025-08-04 22:48:22 +0900

    feat: querydsl을 이용한 정렬과 페이징처리 추가

--- apps/commerce-api/src/main/java/com/loopers/application/provided/ProductFinder.java
+++ apps/commerce-api/src/main/java/com/loopers/application/provided/ProductFinder.java
@@ -2,10 +2,13 @@ package com.loopers.application.provided;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import com.loopers.domain.brand.Brand;
 import com.loopers.domain.product.Product;
+import com.loopers.infrastructure.product.ProductQueryDslRepositoryImpl.ProductWithLikeCount;
 
 public interface ProductFinder {
     Product find(Long productId);
@@ -13,4 +16,6 @@ public interface ProductFinder {
     List<Product> findByConditions(Sort sort);
 
     List<Product> findByBrand(Brand brand);
+
+    Page<ProductWithLikeCount> findWithLikeCount(String sortKey, Pageable pageable);
 }
