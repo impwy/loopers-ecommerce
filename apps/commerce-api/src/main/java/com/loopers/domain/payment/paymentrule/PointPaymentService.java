34d98f17fd5d5d7486287345aa2e2ae528bd1efd
wonyong <pwy6817@gmail.com>
2025-08-22 16:45:35 +0900

    feat: 포인트 결제 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/paymentrule/PointPaymentService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/paymentrule/PointPaymentService.java
@@ -2,16 +2,28 @@ package com.loopers.domain.payment.paymentrule;
 
 import org.springframework.stereotype.Component;
 
+import com.loopers.application.provided.MemberRegister;
+import com.loopers.application.provided.OrderRegister;
+import com.loopers.application.provided.PaymentRegister;
 import com.loopers.domain.member.MemberId;
+import com.loopers.interfaces.api.payment.dto.PaymentV1Dto.Request.PaymentRequest;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Component("POINT")
 @RequiredArgsConstructor
 public class PointPaymentService implements PaymentService {
+    private final OrderRegister orderRegister;
+    private final PaymentRegister paymentRegister;
+    private final MemberRegister memberRegister;
 
+    @Transactional
     @Override
-    public <T> void requestPayment(String orderId, MemberId memberId, T payment) {
+    public void requestPayment(String orderId, MemberId memberId, PaymentRequest payment) {
         // 포인트를 차감한다.
+        memberRegister.usePoint(memberId, payment.totalAmount());
+        orderRegister.successOrder(orderId);
+        paymentRegister.successPayment(orderId);
     }
 }
