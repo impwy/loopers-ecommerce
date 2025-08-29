1a4f3ba0c2292c9860ba90d99f2910014f2459c3
wonyong <pwy6817@gmail.com>
2025-08-29 17:29:43 +0900

    refactor: 포인트 결제 이벤트 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/paymentrule/PointPaymentService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/paymentrule/PointPaymentService.java
@@ -1,39 +1,23 @@
 package com.loopers.domain.payment.paymentrule;
 
-import java.util.List;
-
-import org.springframework.stereotype.Component;
-
-import com.loopers.application.required.MemberRepository;
-import com.loopers.application.required.OrderRepository;
-import com.loopers.application.required.PaymentRepository;
 import com.loopers.domain.member.Member;
+import com.loopers.domain.member.point.PointUsageRequest;
 import com.loopers.domain.order.Order;
-import com.loopers.domain.payment.Payments;
+import com.loopers.domain.payment.PaymentSuccess;
 import com.loopers.interfaces.api.payment.dto.PaymentV1Dto.Request.PaymentRequest;
-
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
 
 @Component("POINT")
 @RequiredArgsConstructor
 public class PointPaymentService implements PaymentService {
-    private final MemberRepository memberRepository;
-    private final OrderRepository orderRepository;
-    private final PaymentRepository paymentRepository;
 
-    @Transactional
+    private final ApplicationEventPublisher eventPublisher;
+
     @Override
     public void requestPayment(Order order, Member member, PaymentRequest payment) {
-        // 포인트를 차감한다.
-        member.usePoint(payment.totalAmount());
-        order.successOrder();
-
-        List<Payments> payments = paymentRepository.findALlByOrderId(order.getOrderNo().value());
-        payments.forEach(Payments::successPayments);
-
-        paymentRepository.saveAll(payments);
-        memberRepository.save(member);
-        orderRepository.save(order);
+        eventPublisher.publishEvent(new PointUsageRequest(member.getMemberId(), payment.totalAmount()));
+        eventPublisher.publishEvent(new PaymentSuccess(order.getOrderNo().value()));
     }
 }
